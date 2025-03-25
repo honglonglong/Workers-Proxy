9e2851493d9d649d22ca25d1dce23e997abb4721
夏至夕阳 <lvjun.summer@live.com>
2025-03-25 19:29:31 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 // Website you intended to retrieve for users.
-const upstream = 'www.google.com'
+const upstream = 'wallabag.onedaywork.site'
 
 // Custom pathname for the upstream website.
 const upstream_path = '/'
 
 // Website you intended to retrieve for users using mobile devices.
-const upstream_mobile = 'www.google.com'
+const upstream_mobile = 'wallabag.onedaywork.site'
 
 // Countries and regions where you wish to suspend your service.
-const blocked_region = ['CN', 'KP', 'SY', 'PK', 'CU']
+const blocked_region = ['US', 'KP', 'SY', 'PK', 'CU']
 
 // IP addresses which you wish to block from using your service.
 const blocked_ip_address = ['0.0.0.0', '127.0.0.1']
 
 // Whether to use HTTPS protocol for upstream address.
-const https = true
+const https = false
 
 // Whether to disable cache.
 const disable_cache = true
